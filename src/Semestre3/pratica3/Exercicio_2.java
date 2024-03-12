ce8df40afdf18bf2748de4a352e07ec213e2f548
Mahen4 <matheushpere@gmail.com>
2024-03-12 20:52:51 -0300

    Refotorado Semestre3/pratica3/Exercicio_2

--- src/Semestre3/pratica3/Exercicio_2.java
+++ src/Semestre3/pratica3/Exercicio_2.java
@@ -5,8 +5,33 @@ desse contexto. Para cada classe, é necessário que ao menos elas possuam cinco a
 relevantes ao contexto que estão aplicadas. Em relação aos métodos, é necessário que elas 
 possuam ao menos três métodos cada*/
 
+import java.util.Scanner;
+import java.util.LinkedList;
+
 public class Exercicio_2 {
 
+}
+
+class Clinica {
+	
+	LinkedList<Paciente> fila = new LinkedList<Paciente>();
+	
+	void atendimentoEmergencia(Paciente paciente) {
+		fila.addFirst(paciente);
+	}
+	
+	void filaEspera(Paciente paciente) {
+		fila.add(paciente);
+	}
+	
+	Paciente proximoPaciente() {
+		return fila.getFirst();
+	}
+	
+	void atendimentoRealizado() {
+		fila.poll();
+	}
+	
 }
 
 class Paciente{
@@ -17,15 +42,73 @@ class Paciente{
 	boolean diagnosticado;
 	boolean estadoDeEmergencia;
 	
+	public Paciente(String cpf, String nome, String possiveisSintomas) {
+		this.cpf = cpf;
+		this.nome = nome;
+		this.sintomas = possiveisSintomas;
+		this.diagnosticado = false;
+		this.estadoDeEmergencia = false;
+	}
+	
+	void triagem(String sintomas) {
+		this.sintomas = sintomas;
+		this.diagnosticado = true;
+	}
+	
+	void emergencia() {
+		this.estadoDeEmergencia = true;
+	}
+	
 }
 
 class Medico{
 	
 	String cpf;
 	String nome;
-	boolean plantao;
-	int salario;
 	String especializacao;
+	int salario;
+	boolean plantao;
+	Clinica clinica;
+	
+	public Medico (String cpf, String nome, String especializacao, int salario) {
+		this.cpf = cpf;
+		this.nome = nome;
+		this.especializacao = especializacao;
+		this.salario = salario;
+		this.plantao = false;
+	}
+	
+	void emPlantao() {
+		this.plantao = true;
+	}
+	
+	void encerrarPlantao() {
+		this.plantao = false;
+	}
+	
+	void Consulta() {
+		System.out.println("Verificando sintomas");
+		System.out.println("Conversando/explicando para o paciente");
+		System.out.println("Preescrevendo indicações");
+		System.out.println("Sanando duvidas restantes");
+	}
+	
+	String checarSintomas() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Os sintomas são:");
+		String sintomas = sc.nextLine();
+		sc.close();
+		return sintomas;
+	}
+	
+	void atenderCliente() {
+		Paciente paciente = clinica.proximoPaciente();
+		if (!paciente.diagnosticado) {
+			paciente.triagem(checarSintomas());
+		}
+		Consulta();
+		clinica.atendimentoRealizado();
+	}
 	
 }
 
@@ -33,8 +116,31 @@ class Atendente{
 	
 	String cpf;
 	String nome;
-	boolean ocupado;
 	int salario;
-	//continuar aqui
+	int agendamentosRealizados;
+	int atendimentosRealizados;
+	Clinica clinica;
+	
+	public Atendente (String cpf, String nome, int salario, Clinica clinica) {
+		this.cpf = cpf;
+		this.nome = nome;
+		this.salario = salario;
+		this.clinica = clinica;
+		this.agendamentosRealizados = 0;
+		this.atendimentosRealizados = 0;
+	}
+	
+	void atenderPaciente(Paciente paciente) {
+		
+		if (paciente.estadoDeEmergencia) {
+			clinica.atendimentoEmergencia(paciente);
+		} else {
+			clinica.filaEspera(paciente);			
+		}
+	}
+	
+	void marcarConsulta() {
+		this.agendamentosRealizados += 1;
+	}
 	
 }
